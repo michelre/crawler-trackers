ed81c10ff956087a74f4e3cfae59a6e2f1df03c4
remimichel <remi.michel38@gmail.com>
2015-01-03 09:08:49 +0100

    little fix

--- src/AppBundle/Doctrine/TorrentDAO.php
+++ src/AppBundle/Doctrine/TorrentDAO.php
@@ -19,7 +19,8 @@ class TorrentDAO {
 
 
     public function createOrUpdate($torrent){
-        if(!empty($torrents = $this->findBySlug($torrent->getSlug())))
+        $torrents = $this->findBySlug($torrent->getSlug());
+        if(!empty($torrents))
             $this->update($torrent, $torrents[0]);
         else
             $this->dm->persist($torrent);
