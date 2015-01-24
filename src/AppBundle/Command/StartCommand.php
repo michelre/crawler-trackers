e78bce2b856f26bdbbe532cedc8c6465dbfba209
remimichel <remi.michel38@gmail.com>
2015-01-24 23:41:47 +0100

    refacto before test

--- src/AppBundle/Command/StartCommand.php
+++ src/AppBundle/Command/StartCommand.php
@@ -24,16 +24,15 @@ class StartCommand extends ContainerAwareCommand{
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $tracker = $input->getArgument('tracker');
-        //$controller = $this->getContainer()->get('main_controller');
-        //$controller->dataAction($tracker, $input->getOption('categories'));
+        $controller = $this->getContainer()->get('main_controller');
+        $controller->dataAction($tracker, $input->getOption('categories'));
         $client = new Client();
         //Ensure that mongo-connector process is stopped
         exec("killall mongo-connector");
         //Delete all torrents in Solr for the right tracker
-        $response = $client->get('http://localhost:8983/solr/collection1/update?commit=true&stream.body=<delete><query>tracker:'. $tracker .'</query></delete>');
-        var_dump($response);
+        $client->get('http://localhost:8983/solr/collection1/update?commit=true&stream.body=<delete><query>tracker:'. $tracker .'</query></delete>');
         //Delete oplogsstatus file and start mongo-connector to replicate data from mongo to Solr and stop mongo-connector
-        //exec("rm -f ~/oplogstatus.txt ; mongo-connector -m localhost:27017 -t http://localhost:8983/solr -d /usr/local/lib/python2.7/dist-packages/mongo_connector/doc_managers/solr_doc_manager.py --oplog-ts ~/oplogstatus.txt");
+        exec("rm -f ~/oplogstatus.txt ; mongo-connector -m localhost:27017 -t http://localhost:8983/solr -d /usr/local/lib/python2.7/dist-packages/mongo_connector/doc_managers/solr_doc_manager.py --oplog-ts ~/oplogstatus.txt");
 
     }
 
